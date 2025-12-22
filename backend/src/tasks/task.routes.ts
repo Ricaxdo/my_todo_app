171fb60ba3fc136ac0b34df7219e105f5d0841fc
Ricardo Castro <ricardo.caroj99@gmail.com>
2025-12-22 11:58:39 -0600

    fix: renames

--- backend/src/tasks/task.routes.ts
+++ backend/src/tasks/task.routes.ts
@@ -1,27 +1,17 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth";
-
-// controllers (los creamos después)
 import {
   createTask,
   deleteTask,
   getTasks,
   updateTask,
-} from "./task.controller";
+} from "../tasks/task.controller";
 
-export const taskRouter = Router();
+export const taskRoutes = Router();
 
-// 🔒 Protege todo
-taskRouter.use(auth);
+taskRoutes.use(auth);
 
-// GET /tasks -> solo del user logueado
-taskRouter.get("/", getTasks);
-
-// POST /tasks -> crea con owner=req.user._id
-taskRouter.post("/", createTask);
-
-// PATCH /tasks/:id -> solo si pertenece al user
-taskRouter.patch("/:id", updateTask);
-
-// DELETE /tasks/:id -> solo si pertenece al user
-taskRouter.delete("/:id", deleteTask);
+taskRoutes.get("/", getTasks);
+taskRoutes.post("/", createTask);
+taskRoutes.put("/:id", updateTask);
+taskRoutes.delete("/:id", deleteTask);
